6f92b70a9d9d666fdb4e2c9d7f5aac19e5b4ab84
joseosso <joseossorio99@hotmail.com>
2025-06-19 14:44:00 -0700

    add tools grid

--- components/sections/tools-section.tsx
+++ components/sections/tools-section.tsx
@@ -1,9 +1,11 @@
 import Section from "@/components/ui/section";
+import ToolsGrid from "./tools-grid";
 
 const ToolsSection = () => {
   return (
-    <Section id="tools">
+    <Section id="tools" className="flex flex-col gap-y-4">
       <h2 className="text-3xl font-semibold text-white">Tools</h2>
+      <ToolsGrid />
     </Section>
   );
 };
--- components/ui/item-card.tsx
+++ components/ui/item-card.tsx
@@ -1,5 +1,30 @@
-const ItemCard = () => {
-  return <div></div>;
+import Image, { StaticImageData } from "next/image";
+
+type ItemCardProps = {
+  img: StaticImageData;
+  imgAlt: string;
+  title: string;
+  description: string;
+};
+
+const ItemCard = ({ img, imgAlt, title, description }: ItemCardProps) => {
+  return (
+    <div className="flex h-48 w-full max-w-3xl overflow-hidden rounded-lg shadow-md">
+      <div className="relative w-1/2">
+        <Image
+          src={img}
+          fill
+          alt={imgAlt}
+          className="object-cover"
+          sizes="(max-width: 768px) 100vw, 50vw"
+        />
+      </div>
+      <div className="w-1/2 p-4 flex flex-col justify-center">
+        <h2 className="text-lg text-white font-semibold mb-2">{title}</h2>
+        <p className="text-sm text-gray-600">{description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ItemCard;
